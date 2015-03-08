633f6864b8b7054e41f2ad2381600d1c6e3b0ffd
Samuel Reed <samuel.trace.reed@gmail.com>
2015-03-08 15:04:38 +0700

    Remove build directory and check-build, instead use npm prepublish.

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 rm -rf ./build
-./node_modules/.bin/6to5 --experimental ./lib --out-dir ./build
+./node_modules/.bin/babel --experimental ./lib --out-dir ./build
+# More cross-platform compatible than `rename`
 find ./build -type f -name '*.jsx' -exec sh -c 'mv -f $0 ${0%.jsx}.js' {} \;
